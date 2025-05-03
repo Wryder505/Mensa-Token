8c2d731d40250e970e1e295419def444ca01103e
W. Ryder <WRyder505@gmail.com>
2025-05-03 06:05:03 -0700

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,5 +3,8 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    localhost: {}
+  },
 };
