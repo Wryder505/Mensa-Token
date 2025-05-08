d94948b7f8e047d0132c09e25d4a151d43c70c35
W. Ryder <WRyder505@gmail.com>
2025-05-08 14:21:04 -0700

    Token Deployment

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -3,13 +3,11 @@ async function main() {
 	const Token = await ethers.getContractFactory("Token") 
 
 	// Deploy contract
-	const token = await Token.deploy()
+	const token = await Token.deploy('Mensa Token', 'MNSA', '1000000')
 	await token.deployed()
 	console.log(`Token Deployed to: ${token.address}`)
 }
  
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
 	.then(() => process.exit(0))
 	.catch((error) => {
